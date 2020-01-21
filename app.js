8fe4ab9e121621b18f3b63d23799da64964a2812
Apple Tech <appletech@Apples-MacBook-Pro.local>
2020-01-21 13:02:51 -0700

    fixed port

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ const mongoose = require("mongoose");
 const bodyParser = require("body-parser");
 const cors = require("cors");
 const app = express();
-const port = env.process.PORT || 5000;
+const port = process.env.PORT || 5000;
 require("dotenv").config();
 
 const Post = require("./routes/post");
